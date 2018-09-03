b5dcd50fa3d1e157954eabd2332ff91492c4f3cc
pimpale <gpimpale29@gmail.com>
2018-09-03 09:14:21 -0700

    added gitignore

--- Release/src/error_methods.d
+++
@@ -1,6 +0,0 @@
-src/error_methods.o: ../src/error_methods.c ../src/constants.h \
- ../src/error_methods.h
-
-../src/constants.h:
-
-../src/error_methods.h:
--- Release/src/glfw_methods.d
+++
@@ -1,6 +0,0 @@
-src/glfw_methods.o: ../src/glfw_methods.c ../src/constants.h \
- ../src/glfw_methods.h
-
-../src/constants.h:
-
-../src/glfw_methods.h:
