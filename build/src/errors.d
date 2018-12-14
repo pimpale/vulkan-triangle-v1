0211526b7b69455460b5bb0c03d2078d2ca9aed2
pimpale <gpimpale29@gmail.com>
2018-12-14 08:48:00 -0800

    removed object and dep files

--- build/src/errors.d
+++
@@ -1,5 +0,0 @@
-src/errors.o: ../src/errors.c ../src/constants.h ../src/errors.h
-
-../src/constants.h:
-
-../src/errors.h:
--- build/src/utils.d
+++
@@ -1,8 +0,0 @@
-src/utils.o: ../src/utils.c ../src/constants.h ../src/errors.h \
- ../src/utils.h
-
-../src/constants.h:
-
-../src/errors.h:
-
-../src/utils.h:
